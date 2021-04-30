ad34560adf3a9d2eef587b43a4e12db15d93cc4b
ciszko <242032@student.pwr.edu.pl>
2021-04-30 20:41:23 +0200

    random join + class

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import os
 from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.utils import get
 import discord
 from dotenv import load_dotenv
 import random
-from scrape import LolCounter
-from tts import TTS
-from anonse import Anonse
+from plugins.scrape import LolCounter
+from plugins.tts import TTS
+from plugins.anonse import Anonse
 from time import sleep
 import platform
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -25,100 +28,119 @@ class MyBot(Bot):
         self.lol_counter = LolCounter()
         self.gtts = TTS()
         self.anonse = Anonse()
+        self.channel_list = []
+
+        self.bg_task = self.loop.create_task(self.my_background_task())
+
+        self.add_commands()
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        while not self.is_closed():
+            for x in self.channel_list:
+                if x.members and str(x.type) == 'voice' and not self.voice_clients:
+                    msg = self.get_random_join_msg()
+                    msg = msg.replace('%user%', random.choice(x.members).name)
+                    tts = self.gtts.create_tts(msg, 'pl')
+                    await self.play_on_channel(None, x, tts)
+            # task runs every 60 seconds
+            await asyncio.sleep(random.randint(10*60, 15*60))
+
+    def get_random_join_msg(self):
+        with open('./glossary/random_join.txt', 'r+', encoding="utf-8") as f:
+            lines = f.readlines()
+        return random.choice(lines)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        msg = message.content.lower()
+
+        greetings = ['cześć bocek', 'czesc bocek',
+                     'czesć bocek', 'cześc bocek']
+
+        if any(x in msg for x in greetings):
+            await message.channel.send(f'Siemano {message.author.name}!')
+
+        elif msg == 'bocek huju':
+            to_choose = ['paruwo', 'obżydronie', 'obszczańcu', 'kutfo']
+            to_choose_2 = [message.author.name, 'ty']
+
+            to_say = f'{random.choice(to_choose_2)} {random.choice(to_choose)}'
+            tts = self.gtts.create_tts(to_say, 'pl')
+            if message.author.voice.channel:
+                await self.play_on_channel(message, message.author.voice.channel, tts)
+            else:
+                await message.channel.send(f'{message.author.name} {random.choice(to_choose)}', tts=True)
+
+        await self.process_commands(message)
+
+    async def play_on_channel(self, ctx=None, voice_channel=None, message=None):
+        vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio(executable=ffmpeg, source=message))
+        # Sleep while audio is playing.
+        while vc.is_connected() and vc.is_playing():
+            await asyncio.sleep(.2)
+        try:
+            await vc.disconnect()
+        except Exception as e:
+            print(e)
+
+        # Delete command after the audio is done playing.
+        if not ctx:
+            return
+        if hasattr(ctx, 'message'):
+            await ctx.message.delete()
+        else:
+            await ctx.delete()
+
+    async def on_ready(self):
+        print(f'{self.user.name} has connected to Discord!')
+        for channel in self.get_all_channels():
+            self.channel_list.append(channel)
+
+    def add_commands(self):
+
+        @self.command(name='siusiak', help='Powie Ci prawdę o siusiaku')
+        async def siusiak(ctx):
+            types = ['potężnego', 'małego', 'brudnego',
+                     'paskudnego', 'ślicznego', 'krzywego', 'obszczanego']
+            response = f'{ctx.author.name} ma {random.choice(types)} siusiaka'
+            await ctx.send(response)
+            await ctx.message.delete()
+
+        @self.command(name='counter', help='Zwraca x kontr na daną postać: $counter jinx x')
+        async def counter(ctx, *arg):
+            counters = self.lol_counter.get_lol_counters(*arg)
+            response = f'**Kontry na {arg[0]}:**\n'
+            response += '\n'.join(f'{x}: {y}' for x, y in counters)
+            await ctx.send(response)
+            await ctx.message.delete()
+
+        @self.command(name='tts', help='Zwraca plik z nagraniem: $tts "tutaj tekst"')
+        async def tts(ctx, arg):
+            tts = self.gtts.create_tts(arg, 'pl')
+
+            await ctx.send(file=discord.File(tts))
+            await ctx.message.delete()
+
+        @self.command(name='anonse', help='Zwraca losowe gejowe anonse')
+        async def anonse(ctx, arg='fetysze'):
+            # Gets voice channel of message author
+            if voice := ctx.author.voice:
+                voice_channel = voice.channel
+                msg = self.anonse.get_anonse(arg)
+                tts = self.gtts.create_tts(msg, 'pl')
+                await self.play_on_channel(ctx, voice_channel, tts)
+            else:
+                msg = (f'{ctx.author.name}, nie jesteś nawet na kanale...')
+                await ctx.channel.send(msg)
 
 
 bot = MyBot(command_prefix='$')
 
 channel_list = []
-
-
-async def play_on_channel(ctx=None, voice_channel=None, message=None):
-    if voice_channel != None:
-        channel = voice_channel.name
-        vc = await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio(executable=ffmpeg, source=message))
-        # Sleep while audio is playing.
-        while vc.is_playing():
-            sleep(.1)
-        await vc.disconnect()
-    else:
-        await ctx.send(f'{ctx.author.name}, nie jesteś nawet na kanale...')
-    # Delete command after the audio is done playing.
-    if hasattr(ctx, 'message'):
-        await ctx.message.delete()
-    else:
-        await ctx.delete()
-
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    for channel in bot.get_all_channels():
-        channel_list.append(channel)
-
-
-@bot.command(name='siusiak', help='Powie Ci prawdę o siusiaku')
-async def siusiak(ctx):
-    types = ['potężnego', 'małego', 'brudnego',
-             'paskudnego', 'ślicznego', 'krzywego', 'obszczanego']
-    response = f'{ctx.author.name} ma {random.choice(types)} siusiaka'
-    await ctx.send(response)
-    await ctx.message.delete()
-
-
-@bot.command(name='counter', help='Zwraca x kontr na daną postać: $counter jinx x')
-async def counter(ctx, *arg):
-    counters = bot.lol_counter.get_lol_counters(*arg)
-    response = f'**Kontry na {arg[0]}:**\n'
-    response += '\n'.join(f'{x}: {y}' for x, y in counters)
-    await ctx.send(response)
-    await ctx.message.delete()
-
-
-@bot.command(name='tts', help='Zwraca plik z nagraniem: $tts "tutaj tekst"')
-async def tts(ctx, arg):
-    tts = bot.gtts.create_tts(arg, 'pl')
-
-    await ctx.send(file=discord.File(tts))
-    await ctx.message.delete()
-
-
-@bot.command(name='anonse', help='Zwraca losowe gejowe anonse')
-async def anonse(ctx, arg='fetysze'):
-    # Gets voice channel of message author
-    voice_channel = ctx.author.voice.channel
-    channel = None
-
-    anonse = bot.anonse.get_random_anonse(arg)
-    tts = bot.gtts.create_tts(anonse, 'pl')
-
-    await play_on_channel(ctx, voice_channel, tts)
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    msg = message.content.lower()
-
-    greetings = ['cześć bocek', 'czesc bocek', 'czesć bocek', 'cześc bocek']
-
-    if any(x in msg for x in greetings):
-        await message.channel.send(f'Siemano {message.author.name}!')
-
-    elif msg == 'bocek huju':
-        to_choose = ['paruwo', 'obżydronie', 'obszczańcu', 'kutfo']
-        to_choose_2 = [message.author.name, 'ty']
-
-        to_say = f'{random.choice(to_choose_2)} {random.choice(to_choose)}'
-        tts = bot.gtts.create_tts(to_say, 'pl')
-        if message.author.voice.channel:
-            await play_on_channel(message, message.author.voice.channel, tts)
-        else:
-            await message.channel.send(f'{message.author.name} {random.choice(to_choose)}', tts=True)
-
-    await bot.process_commands(message)
 
 
 bot.run(TOKEN)
---
+++ update_bocek.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+git fetch
+git checkout -f origin/master
+echo "Bocek was updated" 
