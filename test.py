96aa872e8cc67fcd7176d6529f0b0297aea1dd67
ciszko <242032@student.pwr.edu.pl>
2021-05-03 19:24:10 +0200

    add test 2

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ async def get_all_data():
     url = 'https://192.168.0.31:29999/liveclientdata/allgamedata'
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
         async with session.get(url, timeout=1) as resp:
-            return await resp.json()
+            data = await resp.json()
+            print(data)
 
 if __name__ == '__main__':
     asyncio.run(get_all_data())
