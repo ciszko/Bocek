d365d7a7d54955c0c3570175aa1f528aacd27985
ciszko <242032@student.pwr.edu.pl>
2021-05-19 18:46:29 +0200

    new mp3

--- plugins/tts.py
+++ plugins/tts.py
@@ -2,20 +2,22 @@ import pathlib
 from gtts import gTTS
 import os
 from .common import async_wrap
+from uuid import uuid4
 
 
 class TTS:
     def __init__(self):
-        self.path = pathlib.Path(__file__).parent.absolute()
+        self.path = os.path.join(pathlib.Path(
+            __file__).parent.absolute(), '..', 'mp3')
 
     @async_wrap
     def create_tts(self, text, lang):
         tts = gTTS(text, lang=lang)
-        tts_path = os.path.join(self.path, 'tts.mp3')
+        tts_path = os.path.join(self.path, f'{uuid4().hex[:10]}.mp3')
         tts.save(tts_path)
         return tts_path
 
     @async_wrap
-    def delete_tts(self):
-        os.remove(os.path.join(self.path, 'tts.mp3'))
+    def delete_tts(self, path):
+        os.remove(path)
         return
