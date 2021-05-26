105262a49e5b4c7963596c84adc1660f4aba16b9
ciszko <242032@student.pwr.edu.pl>
2021-05-26 22:31:05 +0200

    glossary

--- plugins/glossary.py
+++ plugins/glossary.py
@@ -1,6 +1,7 @@
 import random
 import json
-from .common import BASEDIR, async_wrap
+from common import BASEDIR
+import asyncio
 
 
 class Glossary:
@@ -8,29 +9,31 @@ class Glossary:
         self.plugin = plugin
         self.glossary_path = f'{BASEDIR}/glossary/{glossary}'
 
-    @async_wrap
     def get_random(self, section='default', **kwargs):
         glossary = self.get_file_json()
-        if glossary:
-            to_ret = random.choice(glossary[section])
-            to_ret = self.replace_placeholders(to_ret, **kwargs)
-            return to_ret
+        if section not in glossary:
+            return None
+        to_ret = random.choice(glossary[section])
+        to_ret = self.replace_placeholders(to_ret, **kwargs)
+        return to_ret
 
-    @async_wrap
-    def get_value(self, section, key, user=None, all_users=None):
+    def get_value(self, section, key, **kwargs):
         glossary = self.get_file_json()
-        if glossary:
-            return glossary[section].get(key)
+        if section not in glossary:
+            return None
+        to_ret = glossary[section].get(key, None)
+        to_ret = self.replace_placeholders(to_ret, **kwargs)
+        return to_ret
 
     def get_file_json(self):
         with open(self.glossary_path, 'r+', encoding='utf-8') as f:
-            data = json.loads(f)
+            data = json.load(f)
         return data
 
-    def replace_placeholders(text, **kwargs):
-        if user in kwargs:
-            text = text.replace('%user%', user)
-        if all_users in kwargs:
-            all_users = ", ".join(all_users)
+    def replace_placeholders(self, text, **kwargs):
+        if 'user' in kwargs:
+            text = text.replace('%user%', kwargs['user'])
+        if 'all_users' in kwargs:
+            all_users = ", ".join(kwargs['all_users'])
             text = text.replace('%all%', all_users)
         return text
