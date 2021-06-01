f1e7ec67fa8431d3bd03d98fb099990d8f3dc642
ciszko <242032@student.pwr.edu.pl>
2021-06-01 23:35:28 +0200

    eloszka

--- plugins/tts.py
+++ plugins/tts.py
@@ -1,23 +1,104 @@
 import pathlib
+from discord.ext import commands
+from discord import File
 from gtts import gTTS
+from google.cloud import texttospeech
 import os
+import random
 from .common import async_wrap
 from uuid import uuid4
+from .log import get_logger
+from typing import Optional
+
+log = get_logger(__name__)
 
 
-class TTS:
-    def __init__(self):
+class Tts(commands.Cog, name='tts'):
+    def __init__(self, bot):
+        self.bot = bot
         self.path = os.path.join(pathlib.Path(
             __file__).parent.absolute(), '..', 'mp3')
+        self.client = texttospeech.TextToSpeechClient()
+
+    @commands.command(name='tts', help='Wysyła plik z nagraniem. $tts "hejo" <pitch=0> <voice=1> <volume=0>')
+    async def tts(self, ctx, text, pitch=0, voice='1', volume=0, lang='pl-PL'):
+        log.info(f'{text}, {pitch}, {voice}, {volume}, {lang}')
+        tts = await self.create_tts(text, pitch, voice, volume)
+
+        await ctx.send(file=File(tts))
+        await ctx.message.delete()
+
+    async def create_tts(self, *args, **kwargs):
+        if 'random' in kwargs:
+            kwargs.pop('random')
+        kwargs = self.get_random_voice()
+        if {'pitch', 'voice', 'volume'}.intersection(kwargs):
+            return await self.tts_google(*args, **kwargs)
+        else:
+            return await self.tts_gtts(*args, **kwargs)
 
     @async_wrap
-    def create_tts(self, text, lang):
+    def tts_gtts(self, text, lang):
+        log.info(text)
         tts = gTTS(text, lang=lang)
         tts_path = os.path.join(self.path, f'{uuid4().hex[:10]}.mp3')
         tts.save(tts_path)
         return tts_path
 
     @async_wrap
+    def tts_google(self, text, lang='pl-PL', pitch=0, voice=1, volume=0):
+        log.info(f'{text}, {lang}, {pitch}, {voice}, {volume}')
+        if not text:
+            return
+
+        if not -20 < pitch < 20:
+            log.warn(f'Wrong pitch value: {pitch}')
+            text = 'Ej człeniu, picz może być od minus dwudziestu do plus dwudziestu'
+
+        if voice not in ['0', '1', '2', '3', 'gothic']:
+            log.warn(f'Wrong voice value: {voice}')
+            text = 'Wybrałeś zły głos'
+            voice = 1
+
+        if type(voice) is str:
+            if voice.lower() == 'gothic':
+                voice = 1
+                pitch = -18
+            else:
+                voice = getattr(texttospeech.SsmlVoiceGender, voice)
+        else:
+            voice = int(voice)
+
+        log.info(f'pitch={pitch}, voice={voice}, volume={volume}')
+
+        text = texttospeech.SynthesisInput(text=text)
+        # voice creation
+        voice = texttospeech.VoiceSelectionParams(
+            language_code=lang, ssml_gender=voice
+        )
+        # additionial params
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3,
+            pitch=pitch,
+            volume_gain_db=volume
+        )
+        # generate response
+        response = self.client.synthesize_speech(
+            input=text, voice=voice, audio_config=audio_config
+        )
+        # save the response
+        tts_path = os.path.join(self.path, f'{uuid4().hex[:10]}.mp3')
+        with open(tts_path, 'wb') as out:
+            out.write(response.audio_content)
+        return tts_path
+
+    @async_wrap
     def delete_tts(self, path):
         os.remove(path)
         return
+
+    def get_random_voice(self, **kwargs):
+        return {**kwargs,
+                'voice': random.randint(0, 3),
+                'pitch': random.randrange(-20, 20)
+                }
