67ec6707d6026aa25c74226f6de7db555b679dc9
ciszko <242032@student.pwr.edu.pl>
2021-06-01 23:35:28 +0200

    eloszka

--- plugins/anonse.py
+++ plugins/anonse.py
@@ -1,4 +1,6 @@
+from os import name
 from bs4 import BeautifulSoup
+from discord.ext import commands
 import requests
 from unidecode import unidecode
 from random import choice, randint
@@ -8,8 +10,9 @@ from .log import get_logger
 log = get_logger(__name__)
 
 
-class Anonse:
-    def __init__(self):
+class Anonse(commands.Cog, name='anonse'):
+    def __init__(self, bot):
+        self.bot = bot
         self.headers = {'User-Agent': 'Bocek/1.0'}
         self.session = requests.Session()
         self.session.headers.update(self.headers)
@@ -28,6 +31,19 @@ class Anonse:
             'praca - dam': '17',
             'widzialem cie': '18',
         }
+
+    @commands.command(name='anonse', help='Zwraca losowe gejowe anonse')
+    async def anonse(self, ctx, arg='fetysze'):
+        # Gets voice channel of message author
+        if voice := ctx.author.voice:
+            voice_channel = voice.channel
+            msg = await self.get_anonse(arg)
+            tts = await self.bot.tts.create_tts(msg, 'pl')
+            await self.bot.play_on_channel(ctx.message, voice_channel, tts)
+        else:
+            msg = (f'{ctx.author.name}, nie jesteś nawet na kanale...')
+            await ctx.channel.send(msg)
+            await ctx.message.delete()
 
     @async_wrap
     def get_anonse(self, cat='fetysze'):
