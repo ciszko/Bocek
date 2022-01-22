985884f4e70bc70b62531692bc4e4d05fe0bb14d
ciszko <242032@student.pwr.edu.pl>
2022-01-23 00:20:48 +0100

    major update

--- plugins/lol_counter.py
+++ plugins/lol_counter.py
@@ -18,9 +18,8 @@ class LolCounter(commands.Cog, name='lol_counter'):
     @commands.command(name='counter', help='Zwraca x kontr na daną postać: $counter jinx x')
     async def counter(self, ctx, *arg):
         counters = await self.get_lol_counters(*arg)
-        response = f'**Kontry na {arg[0]}:**\n```'
-        response += tabulate(
-            [['Champion', '% win'], *counters], headers="firstrow", tablefmt="fancy_grid")
+        response = f'**Kontry na {arg[0]}:**\n```mma\n'
+        response += self.print_tables_side_by_side(counters)
         response += '```'
         await ctx.send(response)
         await ctx.message.delete()
@@ -30,21 +29,51 @@ class LolCounter(commands.Cog, name='lol_counter'):
         url = f'https://www.counterstats.net/league-of-legends/{champion}'
         r = self.session.get(url)
         dom = BeautifulSoup(r.content, 'html.parser')
-        tight_dom = dom.find_all('div', {'class': 'champ-box__wrap new'})[0]
-        picks = tight_dom.findAll('div', {'class': 'champ-box ALL'})
-        best_picks, worst_picks, pop_counters = picks
+        all_h3 = dom.find_all('h3')
+        best_champion_dom = next(
+            x for x in all_h3 if 'Best' in x.get_text()).parent
+        worst_champion_dom = next(
+            x for x in all_h3 if 'Worst' in x.get_text()).parent
+        best_picks = best_champion_dom.find_all(
+            'a', {'class': 'champ-box__row'})
+        worst_picks = worst_champion_dom.find_all(
+            'a', {'class': 'champ-box__row'})
 
-        champs_perc = self.get_champs_and_percentage(best_picks)
-        return champs_perc[:int(limit)]
+        best = self.get_champs_percentage(best_picks)
+        worst = self.get_champs_percentage(worst_picks)
 
-    def get_champs_and_percentage(self, picks):
-        to_ret = []
-        champions = picks.findAll('a')
-        for champ in champions:
-            champ_name = champ['href'].split('/')[-3][3:]
-            if perc := champ.find('span', {'class': 'percentage'}):
-                percentage = perc.get_text()
+        table1 = tabulate([['Champion', 'Punkty', '% win'], *best[:limit]],
+                          headers="firstrow", tablefmt="github", floatfmt=".1f")
+        table2 = tabulate([['Champion', 'Punkty', '% win'], *worst[:limit]],
+                          headers="firstrow", tablefmt="github", floatfmt=".1f")
+
+        return [table1, table2]
+
+    def get_champs_percentage(self, picks):
+        out = []
+        for pick in picks:
+            del1 = 'vs-'
+            l1 = len(del1)
+            vs = pick['href'].find('vs-')
+            del2 = pick['href'][vs + l1:].find('/') + vs + l1
+            champion = (pick['href'][vs + l1:del2])
+            points = pick.find('label').get_text()
+            if not (win_ratio := pick.find('span', {'class': 'b'})):
+                win_ratio = pick.find('span', {'class': 'w'})
+            win_ratio = win_ratio.get_text()
+            out.append([champion, points, win_ratio])
+        return out
+
+    def print_tables_side_by_side(self, tables, spacing=5):
+        final = ''
+        for i, table in enumerate(tables):
+            tables[i] = table.split('\n')
+        lenghts = [len(x) for x in tables]
+        for i in range(max(lenghts)):
+            for table in tables:
+                if i <= len(table):
+                    final += table[i].strip('\n')
+                    final += spacing * ' '
             else:
-                percentage = champ.find('span').find('b').get_text()
-            to_ret.append([champ_name, percentage])
-        return to_ret
+                final += '\n'
+        return final
